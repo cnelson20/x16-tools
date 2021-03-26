fa75f834b19eb37c10ac32070dc2af245f188a23
Cameron Nelson <cjnelson2004@gmail.com>
2021-03-25 22:26:29 -0400

    more modes

--- createPalette.php
+++ createPalette.php
@@ -3,27 +3,38 @@ if ($argc < 3) {
 	echo "usage: php -f createPalette.php input output";
 	exit(1);
 }
+$bit12 = $argc > 3 && $argv[3] == "-12bit";
+if ($bit12) {echo "12bit mode on.";}
 $output = "";
-for ($i = 0; $i < 512; $i++) {$output .= chr(0);}
+for ($i = 0; $i < ($bit12 ? 512 : 160); $i++) {$output .= chr(0);}
 $input = file_get_contents($argv[1]);
 $input = str_replace("\r","",$input);
-$input = explode("\n",$input);
-for( $i = 0; $i < count($input); $i++) {
-	$lines = $input[$i];
-	if ($lines[0] == ";") {
-		unset($input[$i]);
-		$i--;
+$o_input = explode("\n",$input);
+$input = array();
+// var_dump($o_input);
+for( $i = 0; $i < count($o_input); $i++) {
+	$line = $o_input[$i];
+	if (strlen($line) > 0 && $line[0] != ";") {
+		$input[] = $line;
 	}
 }
-$input = array_values($input);
-for ($i = 0; $i < count($input); $i += 3) {
-	for ($j = 0; $j < 3; $j++) {
+// var_dump($input);
+$offset = ($bit12 ? 0 : 1);
+$maxOffset = ($bit12 ? 16 : 3);
+for ($i = 0; $i < count($input); $i += $maxOffset) {
+	for ($j = 0; $j < $maxOffset; $j++) {
 		$color = str_replace("#","",$input[$i+$j]);
-		$red = round(hexdec(substr($color,0,2)) / 16.5);
-		$green = round(hexdec(substr($color,2,2)) / 16.5);
-		$blue = round(hexdec(substr($color,4,2)) / 16.5);
-		$output[$i/3*16+$j+1] = chr($green * 16 + $blue);
-		$output[$i/3*16+$j+257] = chr($red);
+		if ($bit12) {
+			$red = hexdec($color[0]);
+			$green = hexdec($color[1]);
+			$blue = hexdec($color[2]);
+		} else {
+			$red = min(16,round(hexdec(substr($color,0,2)) / 16));
+			$green =  min(16,round(hexdec(substr($color,2,2)) / 16));
+			$blue =  min(16,round(hexdec(substr($color,4,2)) / 16));
+		}
+		$output[$i/$maxOffset*16+$j+$offset] = chr($green * 16 + $blue);
+		$output[$i/$maxOffset*16+$j+$offset+($bit12 ? 256 : 80)] = chr($red);
 
 	}
 }
